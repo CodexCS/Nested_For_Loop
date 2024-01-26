9f2d27dd00dad86e971fbefdbb6e326b62967cae
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2024-01-27 00:26:50 +0530

    5th
    1
    2 2
    3 3 3
    4 4 4 4
    5 5 5 5 5

---
+++ Out7.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Out7 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Out7
+{
+   public static void main(){
+       
+       int p = 1;
+       for(int i = 1; i<=5; i++){
+           for(int j = 1; j<=i;j++ )
+            {
+                
+           System.out.print(i+" ");
+            }
+            System.out.println();
+       }
+       
+   }
+}
