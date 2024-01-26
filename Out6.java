ad390aeff6ca49f250d456e71bbf4891cd62cfcc
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2024-01-26 19:54:38 +0530

    4th Program
    1 2 3 4 5 6 7
    1 2 3 4 5
    1 2 3 
    1

---
+++ Out6.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Out6 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Out6
+{
+    public static void main(){
+        int l = 7;
+        for(int i = 1; i<=4; i++)
+        
+        {
+            for(int j = 1; j<=l; j++)
+            {
+                System.out.print(j +" ");
+                
+            }
+            l = l - 2;
+            System.out.println();
+            
+        }
+    }
+}
