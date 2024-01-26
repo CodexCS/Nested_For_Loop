d5ac2b0f97940d76f43cc9948d3b706f19434cf9
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2024-01-27 00:28:39 +0530

    6th
    1
    2   3
    4   5   6
    7   8   9   10
    11 12 13 14 15

---
+++ Out5.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Out5 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Out5
+{
+    public static void main(){
+        int j = 0;
+        int i = 0;
+        int p = 0;
+        for( i = 1;i<=5;i++){
+            for( j = 1;j<=i;j++){
+                p=p+1;
+                System.out.print(p+" ");
+            }
+           
+            System.out.println();
+        }
+        
+    }
+}
