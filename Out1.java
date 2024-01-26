6282964a3376574ac2c348d01641984c406a0a7b
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2024-01-26 17:10:04 +0530

    This the first one
    1
    1 2 
    1 2 3
    1 2 3 4
    1 2 3 4 5

---
+++ Out1.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Out1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Out1
+{
+    public static void main(){
+        for(int i = 1; i<=5; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j+ " ");
+              //  System.out.println();
+                
+            }
+            System.out.println();
+        }
+    }
+}
