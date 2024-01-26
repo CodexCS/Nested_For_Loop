b45757a60cba73b1002d21535b648c253cab628f
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2024-01-26 17:57:46 +0530

    This is 2nd program
    5 4 3 2 1
    5 4 3 2
    5 4 3 
    5 4
    5

---
+++ Out2.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Out2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Out2
+{
+    public static void main(){
+        for(int i = 1; i<=5; i++)
+        {
+            for(int j = 5; j>=i; j--)
+            {
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
