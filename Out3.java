019797d0acaeb74477966f1d2e2fbf393d99ed92
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2024-01-26 17:59:28 +0530

    This is 3rd program
    5 4 3 2 1
    4 3 2 1 
    3 2 1
    2 1
    1

---
+++ Out3.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Out3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Out3
+{
+    public static void main(){
+        for(int i = 5; i>=1; i--)
+        {
+            for(int j = i; j>=1; j--)
+            {
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+    }
+}
