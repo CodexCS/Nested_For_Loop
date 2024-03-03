f9a1a6bb441d47ebe3c60bc048d21e92cd5c2ac4
Arnav Bhagwat <76836227+CodexCS@users.noreply.github.com>
2024-03-03 20:16:16 +0530

    Add files via upload
    1 
    1 0 
    1 0 1 
    1 0 1 0 
    1 0 1 0 1

---
+++ Out10.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Out10 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Out10
+{
+    public static void main(){
+        for(int i = 1; i<=5; i++){
+            for(int j = 1; j<=i; j++){
+                if(j%2==0){
+                    System.out.print("0"+" ");
+                }else{
+                    System.out.print("1"+" ");
+                }
+                }
+               System.out.println(); 
+            }  
+        }
+    }
+
